bb5c61b2dc82bcd9848dd3003c319cf98a8afeb8
krishnha <r.krishnha@gmail.com>
2025-12-07 01:46:03 +0530

    Add files via upload
    day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloTestLeaf
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to TestLeaf" );
+    }
+}
