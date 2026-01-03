fc03fe76c6a9a2991856e785b3abb8a137d17d8d
krishnha <r.krishnha@gmail.com>
2026-01-03 19:08:17 +0530

    Update HelloTestLeaf.java

--- day1/HelloTestLeaf.java
+++ day1/HelloTestLeaf.java
@@ -8,6 +8,6 @@ public class HelloTestLeaf
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to TestLeaf" );
+        System.out.println( "Welcome To TestLeaf" );
     }
 }
